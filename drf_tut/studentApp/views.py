13680e14d9afd07016154e5f553e2e0f5ba2e1f2
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2022-03-21 19:53:06 +0100

    Assignment: views using mixins and generics

--- drf_tut/studentApp/views.py
+++ drf_tut/studentApp/views.py
@@ -5,8 +5,34 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.http import Http404
 
-# Create your views here.
+from rest_framework import mixins, generics
 
+class StudentList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+
+class StudentDetail(mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, pk):
+        return self.retrieve(request, pk)
+
+    def put(self, request, pk):
+        return self.update(request, pk)
+
+    def delete(self, request, pk):
+        return self.destroy(request,pk)
+
+
+"""
 class StudentList(APIView):
 
     def get(self, request):
@@ -49,3 +75,5 @@ class StudentDetail(APIView):
         student = self.get_object(pk)
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+"""
