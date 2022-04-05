b3cd9197a59d51a87d278c41d41f63919c9f58ee
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2022-04-05 18:58:15 +0200

    authorization

--- drf_tut/nsApp/views.py
+++ drf_tut/nsApp/views.py
@@ -4,14 +4,14 @@ from .serializers import AuthorSerializer, BookSerializer
 from rest_framework import mixins, generics
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 
 
 class AuthorList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
     authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
 
     def get(self, request):
         return self.list(request)
